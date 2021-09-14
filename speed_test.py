2c49e5c89aaf6ef55d8a4deb00b446c7b37cb056
tomvonheill <tomvonheill@gmail.com>
2021-09-14 13:59:08 -0500

    cleaned up file a bit

--- speed_test.py
+++ speed_test.py
@@ -8,15 +8,16 @@ class SpeedClass:
     def __init__(self) -> None:
         self.dl_speed = None
         self.ul_speed = None
+        self.st = speedtest.Speedtest()
     def get_download_speed(self):
         logging.info('checking download speed.....')
-        st = speedtest.Speedtest()
-        self.dl_speed = st.download()/1000000
+        self.st.get_best_server()
+        self.dl_speed = self.st.download()/1000000
         logging.info(f'dl_speed: {self.dl_speed}')
     def get_upload_speed(self):
         logging.info('checking upload speed......')
-        st = speedtest.Speedtest()
-        self.ul_speed = st.upload()/1000000
+        self.st.get_best_server()
+        self.ul_speed = self.st.upload()/1000000
         logging.info(f'dl_speed: {self.ul_speed }')
 
 
@@ -30,10 +31,7 @@ if __name__ == "__main__":
     subprocess_output = subprocess_result.communicate()[0],subprocess_result.returncode
     network_name = subprocess_output[0].decode('utf-8').split('"')[1]
     logging.info(network_name)
-    # st = speedtest.Speedtest()
     start_time = datetime.datetime.now()
-    # dl_speed = st.download()/1000000
-    # up_speed = st.upload()/1000000
     speed_holder = SpeedClass()
     dl_speed= threading.Thread(target=speed_holder.get_download_speed)
     up_speed = threading.Thread(target = speed_holder.get_upload_speed)
@@ -45,9 +43,10 @@ if __name__ == "__main__":
     'download_speed':speed_holder.dl_speed, 
     'upload_speed': speed_holder.ul_speed,
     'network_name': f"'{network_name}'"}
-    query = query.format_map(data)
-    with engine.connect() as con:
-        con.execute(query)
+    if speed_holder.dl_speed is not None:
+        query = query.format_map(data)
+        with engine.connect() as con:
+            con.execute(query)
     end_time = datetime.datetime.now()
     logging.info(end_time-start_time)
 
